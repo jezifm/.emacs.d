fc26a511f064eb77f7b7405a9b1b6c55bb2f2441
Jezrael Arciaga <jezarciaga@gmail.com>
2016-10-18 18:55:26 +0800

    Remove file that shouldn't be tracked

--- elpa/ob-http-20160709.224/ob-http-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; ob-http-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("ob-http-mode.el" "ob-http-pkg.el" "ob-http.el")
-;;;;;;  (22493 23561 663400 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ob-http-autoloads.el ends here

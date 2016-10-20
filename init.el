d2c7c945199f6455cf919546afbee48b37d8371d
Jezrael <jezarciaga@gmail.com>
2016-10-20 23:30:35 +0800

    Disabled mouse and added auto install missing packages

--- init.el
+++ init.el
@@ -7,6 +7,17 @@
 ;; disable full yes no - "Resist the temptation to guess"
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; set function as ctrl
+(setq ns-function-modifier 'control)
+
+;; disable click in emacs
+(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]  
+             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+  (global-unset-key k))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs GUI
@@ -64,6 +75,28 @@
       (expand-file-name "settings" user-emacs-directory))
 (add-to-list 'load-path settings-dir)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install missing dependencies
+(require 'setup-package)
+
+(defun init--install-packages ()
+  (packages-install
+   '(expand-region
+     helm
+     ob-http
+     ob-ipython
+     org-plus-contrib
+     yasnippet
+     key-chord
+     iy-go-to-char
+     magit)))
+
+(condition-case nil
+    (init--install-packages)
+  (error
+   (package-refresh-contents)
+   (init--install-packages)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Expand Region

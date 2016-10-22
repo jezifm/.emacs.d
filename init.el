32a5cff9a9fb050eb1eef513f4dd6b4fc43a39f2
Jezrael <jezarciaga@gmail.com>
2016-10-22 08:24:02 +0800

    Add email

--- init.el
+++ init.el
@@ -7,17 +7,6 @@
 ;; disable full yes no - "Resist the temptation to guess"
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; set function as ctrl
-(setq ns-function-modifier 'control)
-
-;; disable click in emacs
-(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]  
-             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
-             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
-             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
-             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
-  (global-unset-key k))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs GUI
@@ -246,6 +235,7 @@ of `org-babel-temporary-directory'."
 
 ;; Set author
 (setq user-full-name "Jezrael Arciaga")
+(setq user-mail-address "jezarciaga@gmail.com")
 
 ;; enable markdown export
 (eval-after-load "org"

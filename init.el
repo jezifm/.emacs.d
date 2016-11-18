5de74264660613e5fa54fc62968355d7c67e1333
Jezrael Arciaga <jezarciaga@gmail.com>
2016-11-18 14:58:38 +0800

    Enable paredit

--- init.el
+++ init.el
@@ -114,7 +114,9 @@
      swiper
      avy
      swift-mode
-     color-theme-modern)))
+     color-theme-modern
+     paredit
+     )))
 
 (condition-case nil
     (init--install-packages)
@@ -585,3 +587,16 @@ of `org-babel-temporary-directory'."
 
 (require 'avy)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Paredit
+
+(require 'paredit)
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)

6a7961174135ae995df42ef374718a9179c07027
Jezrael Arciaga <jezarciaga@gmail.com>
2018-07-24 14:15:01 +0800

    Remove global default truncate lines to `t`

--- init.el
+++ init.el
@@ -28,7 +28,6 @@
 
 ;; toggle text wrap
 (global-set-key (kbd "C-c t") 'toggle-truncate-lines)
-(set-default 'truncate-lines t)
 
 ;; fix issue - annoying save-abbrevs prompt
 (setq save-abbrevs 'silently)

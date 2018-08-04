aa6e3aa45765a0669c1e6d72dc6ae41d2e6bd021
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-05 06:05:37 +0800

    Implement hydra on window-numbering

--- init.el
+++ init.el
@@ -390,22 +390,76 @@ Version 2017-09-01"
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Hydra
+
+(use-package hydra
+  :ensure t
+  :defer t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Window Numbering
 
 (use-package window-numbering
   :ensure t
   :defer t
   :bind (("M-1" . select-window-1)
-         ("M-2" . select-window-2))
+         ("M-2" . select-window-2)
+         ("M-0" . hydra-window-stuff/body))
   :commands window-numbering-remove-keymap
   :config
   (window-numbering-mode 1)
+  (define-key window-numbering-keymap (kbd "M-0") nil)
+
   (defun window-numbering-remove-keymap ()
     "Fix keymap conflict with `magit'"
     (interactive)
     (mapc
      (lambda (num) (define-key magit-mode-map (kbd (format "M-%s" num)) nil))
      (number-sequence 1 5)))
+
+  (defhydra hydra-window-stuff (:hint nil)
+    "
+          Split: _v_ert  _s_:horz
+         Delete: _c_lose  _o_nly
+  Switch Window: _h_:left  _j_:down  _k_:up  _l_:right
+        Buffers: _p_revious  _n_ext  _b_:select  _f_ind-file  _F_projectile
+         Winner: _u_ndo  _r_edo
+         Resize: _H_:splitter left  _J_:splitter down  _K_:splitter up  _L_:splitter right
+           Move: _a_:up  _z_:down  _i_menu"
+
+
+    ("z" scroll-up-line)
+    ("a" scroll-down-line)
+    ("i" idomenu)
+
+    ("u" winner-undo)
+    ("r" winner-redo)
+
+    ("h" windmove-left)
+    ("j" windmove-down)
+    ("k" windmove-up)
+    ("l" windmove-right)
+
+    ("p" previous-buffer)
+    ("n" next-buffer)
+    ("b" ido-switch-buffer)
+    ("f" ido-find-file)
+    ("F" projectile-find-file)
+
+    ("s" split-window-below)
+    ("v" split-window-right)
+
+    ("c" delete-window)
+    ("o" delete-other-windows)
+
+    ("H" hydra-move-splitter-left)
+    ("J" hydra-move-splitter-down)
+    ("K" hydra-move-splitter-up)
+    ("L" hydra-move-splitter-right)
+
+    ("q" nil))
+
   :hook (magit-mode . window-numbering-remove-keymap))
 
 

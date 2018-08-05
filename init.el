6de95a9b5d074a9e3853eb5c7dd292ab0d9bf38d
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-05 18:05:27 +0800

    Add hydra for python outline

--- init.el
+++ init.el
@@ -820,7 +820,8 @@ of `org-babel-temporary-directory'."
          ("\\.tac\\'" . python-mode))
   :bind (
          :map python-mode-map
-         ("<tab>" . indent-for-tab-command))
+         ("<tab>" . indent-for-tab-command)
+         ("C-c #" . hydra-python-outline/body))
 
   :config
   (setq truncate-lines t)
@@ -836,6 +837,39 @@ of `org-babel-temporary-directory'."
     (setq-local outline-regexp "[ \t]*# \\|[ \t]+\\(class\\|def\\|if\\|elif\\|else\\|while\\|for\\|try\\|except\\|with\\) ")
     (setq-local outline-level 'py-outline-level)
     (setq-local outshine-use-speed-commands nil))
+
+  (defhydra hydra-python-outline (:hint nil)
+    "
+^Hide^             ^Show^           ^Move
+^^^^^^------------------------------------------------------
+_q_: sublevels     _a_: all         _u_: up
+_t_: body          _e_: entry       _n_: next visible
+_o_: other         _i_: children    _p_: previous visible
+_c_: entry         _k_: branches    _f_: forward same level
+_l_: leaves        _s_: subtree     _b_: backward same level
+_d_: subtree   _<tab>_: cycle
+"
+    ;; Hide
+    ("q" hide-sublevels)    ; Hide everything but the top-level headings
+    ("t" hide-body)         ; Hide everything but headings (all body lines)
+    ("o" hide-other)        ; Hide other branches
+    ("c" hide-entry)        ; Hide this entry's body
+    ("l" hide-leaves)       ; Hide body lines in this entry and sub-entries
+    ("d" hide-subtree)      ; Hide everything in this entry and sub-entries
+    ("<tab>" outline-cycle) ;
+    ;; Show
+    ("a" show-all)          ; Show (expand) everything
+    ("e" show-entry)        ; Show this heading's body
+    ("i" show-children)     ; Show this heading's immediate child sub-headings
+    ("k" show-branches)     ; Show all sub-headings under this heading
+    ("s" show-subtree)      ; Show (expand) everything in this heading & below
+    ;; Move
+    ("u" outline-up-heading)                ; Up
+    ("n" outline-next-visible-heading)      ; Next
+    ("p" outline-previous-visible-heading)  ; Previous
+    ("f" outline-forward-same-level)        ; Forward - same level
+    ("b" outline-backward-same-level)       ; Backward - same level
+    ("z" nil "leave"))
 
   :hook (python-mode . outshine-python-mode-hook))
 

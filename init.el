1ed7b6abdfda9adee8c8b5dd3138014bc91c62ed
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-05 23:09:51 +0800

    Use auto generated doc for python hydra

--- init.el
+++ init.el
@@ -850,38 +850,27 @@ of `org-babel-temporary-directory'."
     (setq-local outline-level 'py-outline-level)
     (setq-local outshine-use-speed-commands nil))
 
-  (defhydra hydra-python-outline (:hint nil)
-    "
-^Hide^             ^Show^           ^Move
-^^^^^^------------------------------------------------------
-_q_: sublevels     _a_: all         _u_: up
-_t_: body          _e_: entry       _n_: next visible
-_o_: other         _i_: children    _p_: previous visible
-_c_: entry         _k_: branches    _f_: forward same level
-_l_: leaves        _s_: subtree     _b_: backward same level
-_d_: subtree   _<tab>_: cycle
-"
+  (defhydra hydra-python-outline (:hint nil :columns 3)
+    "Python Outline"
     ;; Hide
-    ("q" hide-sublevels)    ; Hide everything but the top-level headings
-    ("t" hide-body)         ; Hide everything but headings (all body lines)
-    ("o" hide-other)        ; Hide other branches
-    ("c" hide-entry)        ; Hide this entry's body
-    ("l" hide-leaves)       ; Hide body lines in this entry and sub-entries
-    ("d" hide-subtree)      ; Hide everything in this entry and sub-entries
-    ("<tab>" outline-cycle) ;
+    ("<up>" outline-move-subtree-up "outline-move-subtree-up")
+    ("<down>" outline-move-subtree-down "outline-move-subtree-down")
+    ("<left>" outline-cycle "outline-cycle")
+    ("<right>" outline-cycle "outline-cycle")
+    ("<tab>" outline-cycle "outline-cycle")
     ;; Show
-    ("a" show-all)          ; Show (expand) everything
-    ("e" show-entry)        ; Show this heading's body
-    ("i" show-children)     ; Show this heading's immediate child sub-headings
-    ("k" show-branches)     ; Show all sub-headings under this heading
-    ("s" show-subtree)      ; Show (expand) everything in this heading & below
+    ("a" show-all "show-all")
+    ("e" show-entry "show-entry")
+    ("i" show-children "show-children")
+    ("k" show-branches "show-branches")
+    ("s" show-subtree "show-subtree")
     ;; Move
-    ("u" outline-up-heading)                ; Up
-    ("n" outline-next-visible-heading)      ; Next
-    ("p" outline-previous-visible-heading)  ; Previous
-    ("f" outline-forward-same-level)        ; Forward - same level
-    ("b" outline-backward-same-level)       ; Backward - same level
-    ("z" nil "leave"))
+    ("u" outline-up-heading "outline-up-heading")
+    ("n" outline-next-visible-heading "outline-next-visible-heading")
+    ("p" outline-previous-visible-heading "outline-previous-visible-heading")
+    ("f" outline-forward-same-level "outline-forward-same-level")
+    ("b" outline-backward-same-level "outline-backward-same-level")
+    ("q" nil "leave"))
 
   :hook (python-mode . outshine-python-mode-hook))
 

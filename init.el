f2548d75ba301a6b204e1cbff174f65c5b4c8df0
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-05 19:16:32 +0800

    Merge helm google inside helm

--- init.el
+++ init.el
@@ -582,9 +582,9 @@ Version 2017-09-01"
         helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
         helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
         helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-        helm-ff-file-name-history-use-recentf t))
+        helm-ff-file-name-history-use-recentf t)
   (when (executable-find "curl")
-    (setq helm-google-suggest-use-curl-p t))
+    (setq helm-google-suggest-use-curl-p t)))
 
 (use-package helm-dash :ensure t :defer t)
 

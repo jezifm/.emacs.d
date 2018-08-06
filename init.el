c1d307fe2cd0b200e5a36ae5cbfee85494a096a0
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-07 04:42:58 +0800

    Configure newsreader use `gnus`

--- init.el
+++ init.el
@@ -945,7 +945,16 @@ of `org-babel-temporary-directory'."
 (use-package gnus
   :ensure t
   :defer t
-  :hook (gnus-group-mode . gnus-topic-mode))
+  :config
+  (setq gnus-select-method
+        '(nnimap "gmail"
+	         (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
+	         (nnimap-server-port "imaps")
+	         (nnimap-stream ssl)))
+
+  (setq smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587
+        gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

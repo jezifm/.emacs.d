94b4a499994e9822e50c61efd16492ddc3e282f0
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-09 18:25:34 +0800

    Add `vr/mc-mark` to list of run-once

--- init.el
+++ init.el
@@ -731,9 +731,11 @@ of `org-babel-temporary-directory'."
     (push func mc--default-cmds-to-run-for-all)
     (remove-duplicates mc--default-cmds-to-run-once))
 
+  ;; run once
   (jez/mc-add-cmds-once 'jez/mark-multiple)
+  (jez/mc-add-cmds-once 'vr/mc-mark)
+  ;; run to all cursor
   (jez/mc-add-cmds-all 'org-self-insert-command)
-  ;; paredit
   (jez/mc-add-cmds-all 'paredit-backward-kill-word)
   (jez/mc-add-cmds-all 'paredit-open-parenthesis)
   (jez/mc-add-cmds-all 'paredit-open-round)

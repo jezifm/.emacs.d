9e111eb8fea77d8fcba61316a219c85c60b013b3
Jezrael Arciaga <jezarciaga@gmail.com>
2020-05-01 16:25:57 +0800

    Add function delete-this-file

--- init.el
+++ init.el
@@ -283,6 +283,16 @@ Note: just like `align-regexp' but better"
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil))))))
 
+(defun delete-this-file ()
+  "Delete the current file, and kill the buffer."
+  (interactive)
+  (unless (buffer-file-name)
+    (error "No file is currently being edited"))
+  (when (yes-or-no-p (format "Really delete '%s'?"
+                             (file-name-nondirectory buffer-file-name)))
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
 (defun jez-camelize (s)
   "Convert under_score string S to CamelCase string."
   (mapconcat 'identity (mapcar

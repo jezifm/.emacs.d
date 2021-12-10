4b86ec3c27f20641be93ec2325a81c75b85f6f09
Jezrael Arciaga <jezarciaga@gmail.com>
2021-12-10 21:16:24 +0800

    Fix error plantuml-indent-regexp-start does not exists

--- init.el
+++ init.el
@@ -1865,26 +1865,27 @@ on delete cascade;"
   (setq org-plantuml-jar-path "~/.emacs.d/elpa/contrib/scripts/plantuml.jar")
   (setq plantuml-jar-path "~/.emacs.d/elpa/contrib/scripts/plantuml.jar")
   (setq plantuml-default-exec-mode 'jar)
-  (setq plantuml-indent-regexp-start (list plantuml-indent-regexp-block-start
-                                           plantuml-indent-regexp-group-start
-                                           plantuml-indent-regexp-activate-start
-                                           plantuml-indent-regexp-box-start
-                                           plantuml-indent-regexp-ref-start
-                                           plantuml-indent-regexp-legend-start
-                                           plantuml-indent-regexp-note-start
-                                           plantuml-indent-regexp-newif-start
-                                           plantuml-indent-regexp-loop-start
-                                           plantuml-indent-regexp-fork-start
-                                           plantuml-indent-regexp-title-start
-                                           plantuml-indent-regexp-header-start
-                                           plantuml-indent-regexp-footer-start
-                                           plantuml-indent-regexp-macro-start
-                                           plantuml-indent-regexp-oldif-start
-                                           plantuml-indent-regexp-user-control-start
-                                           ".*!foreach.*$"
-                                           ".*!procedure.*$"
-                                           "^ *repeat *$"
-                                           ))
+  (setq plantuml-indent-regexp-start (list
+                                      ;; plantuml-indent-regexp-block-start
+                                      plantuml-indent-regexp-group-start
+                                      plantuml-indent-regexp-activate-start
+                                      plantuml-indent-regexp-box-start
+                                      plantuml-indent-regexp-ref-start
+                                      plantuml-indent-regexp-legend-start
+                                      plantuml-indent-regexp-note-start
+                                      plantuml-indent-regexp-newif-start
+                                      plantuml-indent-regexp-loop-start
+                                      plantuml-indent-regexp-fork-start
+                                      plantuml-indent-regexp-title-start
+                                      plantuml-indent-regexp-header-start
+                                      plantuml-indent-regexp-footer-start
+                                      plantuml-indent-regexp-macro-start
+                                      plantuml-indent-regexp-oldif-start
+                                      plantuml-indent-regexp-user-control-start
+                                      ".*!foreach.*$"
+                                      ".*!procedure.*$"
+                                      "^ *repeat *$"
+                                      ))
   (setq plantuml-indent-regexp-end (list plantuml-indent-regexp-block-end
                                          plantuml-indent-regexp-group-end
                                          plantuml-indent-regexp-activate-end
